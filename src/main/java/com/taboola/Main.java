ae9dac1106c65f0be785e00ed9998c93207b4e37
Dav He <s991179@gmail.com>
2024-12-27 06:16:35 +0800

    feat: print

--- src/main/java/com/taboola/Main.java
+++ src/main/java/com/taboola/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
+        System.out.printf("ThreadTest1 with sync: ");
         ThreadTest1.testMethodSync();
+        System.out.printf("ThreadTest1 without sync: ");
         ThreadTest1.testMethodNoSync();
     }
 }
